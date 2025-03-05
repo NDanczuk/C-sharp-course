05ad09dcd6e556d3b6d6cf474f7fcb41673d030c
NDanczuk <nicolasdanc@gmail.com>
2025-03-05 12:11:41 -0300

    add files

---
+++ Course/Course/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course { 
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            bool complete = false;
+            char gender = 'f';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string name = "Nicolas";
+            double saldo = 10.3412341;
+            int age = 19;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(complete);
+            Console.WriteLine(gender);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(name);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("{0} is {1} years and has R${2:F2}", name, age, saldo);
+
+            Console.WriteLine($"{name} is {age} years and ahs R${saldo:F2}");
+        }
+    }
+}
---
+++ Products/Products/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product1 = "Computer";
+            string product2 = "Office table";
+
+            byte age = 30;
+            int code = 5290;
+            char gender = 'M';
+
+            double price1 = 2100.0;
+            double price2 = 650.50;
+            double value = 53.234567;
+
+            Console.WriteLine("Products: ");
+            Console.WriteLine($"{product1}, wich costs ${price1}");
+            Console.WriteLine($"{product2}, wich costs ${price2}");
+
+            Console.WriteLine($"Register: {age} years, code: {code} & gender: {gender}");
+
+            Console.WriteLine($"Value with 8 decimal places: {value:F8}");
+            Console.WriteLine($"Value with 3 decimal places: {value:F3}");
+            Console.WriteLine($"Value with invariant culture: {value.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
